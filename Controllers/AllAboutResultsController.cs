6cfa6aacb9db33120d02c872272698975883c23b
Doug <70054506+smykad@users.noreply.github.com>
2022-10-30 04:21:07 -0400

    hands on #2

---
+++ Controllers/AllAboutResultsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace InTheBag.Controllers
+{
+    public class AllAboutResultsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var weekday = DateTime.Now.DayOfWeek;
+            var day = weekday.ToString();
+            var time = DateTime.Now.Hour;
+
+            // day = "Monday";
+            // day = "Tuesday";
+            // day = "Wednesday";
+            // day = "Thursday";
+            // day = "Friday";
+            // day = "Saturday";
+            // day = "Sunday";
+
+            if (time <= 6)
+            {
+                ViewBag.Greeting = "It is too early to be up!";
+            }
+            else if (time <= 12)
+            {
+                ViewBag.Greeting = "Good Morning";
+            }
+            else if (time <= 18)
+            {
+                ViewBag.Greeting = "Good Afternoon";
+            }
+            else
+            {
+                ViewBag.Greeting = "Good evening";
+            }
+            int route = 0;
+
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                    ViewData["dayMessage"] = "The work week is just started! stay focused, you have a lot to do this week!";
+                    route = 1;
+                    break;
+                case "Wednesday":
+                    ViewData["dayMessage"] = "Halfway to the weekend!";
+                    route = 2;
+                    break;
+                case "Thursday":
+                    ViewData["dayMessage"] = "I never could get the hang of Thursdays";
+                    route = 3;
+                    break;
+                case "Friday":
+                    ViewData["dayMessage"] = "Woo hoo TGIF";
+                    route = 4;
+                    break;
+                default:
+                    ViewData["dayMessage"] = "Ahhhh the weekend!";
+                    route = 5;
+                    break;
+            }
+            if (route == 1)
+            {
+                return RedirectToAction("Weekday", "AllAboutResults");
+            }
+            else if (route == 2 || route == 3)
+            {
+                return Redirect("https://lisabalbach.com/CIT218/Chapter8/HappyWednesday.html");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        public IActionResult Weekday()
+        {
+            ViewBag.Greeting = "Congratulations, the work week just started and you have been rerouted";
+            return View();
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,6 +21,34 @@ namespace InTheBag.Controllers
         {
             return View();
         }
+
+        public IActionResult IndexViewBag() 
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("Peace");
+            WishList.Add("Health");
+            WishList.Add("Happiness");
+            ViewBag.WishList = WishList;
+            return View(); 
+        }
+        public IActionResult IndexViewData() 
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("Quies");
+            WishList.Add("Salutem");
+            WishList.Add("Beatitudinem");
+            ViewData["WishList"] = WishList;
+            return View(); 
+        }
+        public IActionResult IndexTempData() 
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("La Paz");
+            WishList.Add("La Salud");
+            WishList.Add("La Felicidad");
+            TempData["WishList"] = WishList;
+            return View(); 
+        }
         public IActionResult Privacy()
         {
             return View();
