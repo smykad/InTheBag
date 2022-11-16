e8ed0c9b4cd504241f4e114e88a2c5ea0b399f39
Doug <70054506+smykad@users.noreply.github.com>
2022-11-16 03:28:34 -0500

    handson#3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,10 +35,19 @@ namespace InTheBag.Controllers
         {
             return View();
         }
-        [HttpPost]
+        /*[HttpPost]
         public IActionResult NewWishIndex(Wishes model)
         {
             Wishes myWishes = new Wishes { ID = 2, wish1 = model.wish1, wish2 = model.wish2, wish3 = model.wish3 };
+            string jsonWishes = JsonConvert.SerializeObject(myWishes);
+            HttpContext.Session.SetString("wish", jsonWishes);
+            return View("WishIndex");
+        }*/
+
+        [HttpPost]
+        public IActionResult NewWishIndex(int? ID)
+        {
+            Wishes myWishes = new Wishes { ID = 2, wish1 = Request.Form["wish1"], wish2 = Request.Form["wish2"], wish3 = Request.Form["wish3"] };
             string jsonWishes = JsonConvert.SerializeObject(myWishes);
             HttpContext.Session.SetString("wish", jsonWishes);
             return View("WishIndex");
