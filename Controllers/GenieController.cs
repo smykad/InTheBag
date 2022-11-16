fb6a5d05f90b53bc87261e79be56516c8901fbd2
Doug <70054506+smykad@users.noreply.github.com>
2022-11-16 03:20:34 -0500

    genie-controller

---
+++ Controllers/GenieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InTheBag.Controllers
+{
+    public class GenieController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string GenieName, int Age, int WishesGranted)
+        {
+            if (WishesGranted > 5000 || Age > 1000)
+                return View("ExperiencedGenie");
+            else
+                return View("Novice");
+        }
+    }
+}
